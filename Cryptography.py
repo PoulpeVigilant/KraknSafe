e8861a34cd2ddd22086af2661ec6a6efcb129c08
PoulpeVigilant <planvigipoulpe@gmail.com>
2025-03-11 14:13:20 +0100

    Wondering the crypto part

--- Cryptography.py
+++ Cryptography.py
@@ -1,6 +1,8 @@
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
+print(key)
+print("---------------------------------------------")
 cipher_suite = Fernet(key)
 cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
 print(cipher_text)
